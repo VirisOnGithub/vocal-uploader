02ec5efdfd826fd74c108db8691435c40f2cc0da
Clément <clement.reniers@etu.univ-lyon1.fr>
2025-02-16 22:17:58 +0100

    added scalar module

--- front/nuxt.config.ts
+++ front/nuxt.config.ts
@@ -2,9 +2,17 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui', '@nuxtjs/i18n'],
+  modules: ['@nuxt/ui', '@nuxtjs/i18n', '@scalar/nuxt'],
     i18n: {
       vueI18n: './i18n.config.ts',
         defaultLocale: 'en',
+    },
+    scalar: {
+        spec: {
+            url: 'docs/openapi.yaml',
+        },
+        pathRouting: {
+            basePath: '/scalar',
+        },
     }
 })
